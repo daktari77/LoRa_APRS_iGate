40f188e81676c13f933c8f3bba056d6c4ff668e2
richonguzman <richon2001@hotmail.com>
2024-05-30 13:38:55 -0400

    Syslog cleaning

--- src/syslog_utils.cpp
+++ src/syslog_utils.cpp
@@ -16,52 +16,53 @@ namespace SYSLOG_Utils {
         String syslogPacket = "<165>1 - " + Config.callsign + " CA2RXU_LoRa_iGate_1.3" + " - - - "; //RFC5424 The Syslog Protocol
         switch (type) {
             case 0:     // CRC
-                syslogPacket += type + " / CRC-ERROR / " + rxPacket.packet;
+                syslogPacket += "CRC / CRC-ERROR / " + rxPacket.packet;
                 syslogPacket += " / " + String(rxPacket.rssi) + "dBm / " + String(rxPacket.snr) + "dB / " + String(rxPacket.freqError) + "Hz";
                 break;
             case 1:     // RX
+                syslogPacket += "RX / ";
                 if (rxPacket.packet.indexOf("::") > 10) {
-                    syslogPacket += type + " / MESSAGE / " + rxPacket.packet.substring(3, rxPacket.packet.indexOf(">")) + " ---> " + rxPacket.packet.substring(rxPacket.packet.indexOf("::")+2);
+                    syslogPacket += "MESSAGE / " + rxPacket.packet.substring(3, rxPacket.packet.indexOf(">")) + " ---> " + rxPacket.packet.substring(rxPacket.packet.indexOf("::") + 2);
                     syslogPacket += " / " + String(rxPacket.rssi) + "dBm / " + String(rxPacket.snr) + "dB / " + String(rxPacket.freqError) + "Hz";
                 } else if (rxPacket.packet.indexOf(":!") > 10 || rxPacket.packet.indexOf(":=") > 10) {
-                    syslogPacket += type + " / GPS / " + rxPacket.packet.substring(3,rxPacket.packet.indexOf(">")) + " / ";
+                    syslogPacket += "GPS / " + rxPacket.packet.substring(3, rxPacket.packet.indexOf(">")) + " / ";
                     if (rxPacket.packet.indexOf("WIDE1-1") > 10) {
-                        syslogPacket += rxPacket.packet.substring(rxPacket.packet.indexOf(">")+1,rxPacket.packet.indexOf(",")) + " / WIDE1-1 / ";
+                        syslogPacket += rxPacket.packet.substring(rxPacket.packet.indexOf(">") + 1, rxPacket.packet.indexOf(",")) + " / WIDE1-1 / ";
                     } else {
-                        syslogPacket += rxPacket.packet.substring(rxPacket.packet.indexOf(">")+1,rxPacket.packet.indexOf(":")) + " / - / ";
+                        syslogPacket += rxPacket.packet.substring(rxPacket.packet.indexOf(">") + 1, rxPacket.packet.indexOf(":")) + " / - / ";
                     }
                     syslogPacket += String(rxPacket.rssi) + "dBm / " + String(rxPacket.snr) + "dB / " + String(rxPacket.freqError) + "Hz / " +  GPS_Utils::getDistanceAndComment(rxPacket.packet);
                 } else if (rxPacket.packet.indexOf(":>") > 10) {
-                    syslogPacket += type + " / STATUS / " + rxPacket.packet.substring(3,rxPacket.packet.indexOf(">")) + " ---> " + rxPacket.packet.substring(rxPacket.packet.indexOf(":>")+2);
+                    syslogPacket += "STATUS / " + rxPacket.packet.substring(3, rxPacket.packet.indexOf(">")) + " ---> " + rxPacket.packet.substring(rxPacket.packet.indexOf(":>") + 2);
                     syslogPacket += " / " + String(rxPacket.rssi) + "dBm / " + String(rxPacket.snr) + "dB / " + String(rxPacket.freqError) + "Hz";
                 } else if (rxPacket.packet.indexOf(":`") > 10) {
-                    syslogPacket += type + " / MIC-E / " + rxPacket.packet.substring(3,rxPacket.packet.indexOf(">")) + " ---> " + rxPacket.packet.substring(rxPacket.packet.indexOf(":`")+2);
+                    syslogPacket += "MIC-E / " + rxPacket.packet.substring(3, rxPacket.packet.indexOf(">")) + " ---> " + rxPacket.packet.substring(rxPacket.packet.indexOf(":`") + 2);
                     syslogPacket += " / " + String(rxPacket.rssi) + "dBm / " + String(rxPacket.snr) + "dB / " + String(rxPacket.freqError) + "Hz";
                 } else if (rxPacket.packet.indexOf(":T#") >= 10 && rxPacket.packet.indexOf(":=/") == -1) {
-                    syslogPacket += type + " / TELEMETRY / " + rxPacket.packet.substring(3,rxPacket.packet.indexOf(">")) + " ---> " + rxPacket.packet.substring(rxPacket.packet.indexOf(":T#")+3);
+                    syslogPacket += "TELEMETRY / " + rxPacket.packet.substring(3, rxPacket.packet.indexOf(">")) + " ---> " + rxPacket.packet.substring(rxPacket.packet.indexOf(":T#") + 3);
                     syslogPacket += " / " + String(rxPacket.rssi) + "dBm / " + String(rxPacket.snr) + "dB / " + String(rxPacket.freqError) + "Hz";
                 } else if (rxPacket.packet.indexOf(":;") > 10) {
-                    syslogPacket += type + " / OBJECT / " + rxPacket.packet.substring(3,rxPacket.packet.indexOf(">")) + " ---> " + rxPacket.packet.substring(rxPacket.packet.indexOf(":;")+2);
+                    syslogPacket += "OBJECT / " + rxPacket.packet.substring(3, rxPacket.packet.indexOf(">")) + " ---> " + rxPacket.packet.substring(rxPacket.packet.indexOf(":;") + 2);
                     syslogPacket += " / " + String(rxPacket.rssi) + "dBm / " + String(rxPacket.snr) + "dB / " + String(rxPacket.freqError) + "Hz";
                 } else {
-                    syslogPacket += type + " / " + rxPacket.packet;
+                    syslogPacket += rxPacket.packet;
                     syslogPacket += " / " + String(rxPacket.rssi) + "dBm / " + String(rxPacket.snr) + "dB / " + String(rxPacket.freqError) + "Hz";
                 }
                 break;
             case 2:     // APRSIS TX
                 if (rxPacket.packet.indexOf(":>") > 10) {
-                    syslogPacket += type + " / StartUp_Status / " + rxPacket.packet.substring(rxPacket.packet.indexOf(":>")+2);
+                    syslogPacket += "APRSIS TX / StartUp_Status / " + rxPacket.packet.substring(rxPacket.packet.indexOf(":>") + 2);
                 } else {
-                    syslogPacket += type + " / QUERY / " + rxPacket.packet;
+                    syslogPacket += "APRSIS TX / QUERY / " + rxPacket.packet;
                 }
                 break;
             case 3:     // TX
                 if (rxPacket.packet.indexOf("RFONLY") > 10) {
-                    syslogPacket += type + " / RFONLY / " + rxPacket.packet;
+                    syslogPacket += "LORA TX / RFONLY / " + rxPacket.packet;
                 } else if (rxPacket.packet.indexOf("::") > 10) {
-                    syslogPacket += type + " / MESSAGE / " + rxPacket.packet.substring(0,rxPacket.packet.indexOf(">")) + " ---> " + rxPacket.packet.substring(rxPacket.packet.indexOf("::")+2);
+                    syslogPacket += "LORA TX / MESSAGE / " + rxPacket.packet.substring(0, rxPacket.packet.indexOf(">")) + " ---> " + rxPacket.packet.substring(rxPacket.packet.indexOf("::") + 2);
                 } else {
-                    syslogPacket += type + " / " + rxPacket.packet;
+                    syslogPacket += "LORA TX / " + rxPacket.packet;
                 }
                 break;                
             default:
